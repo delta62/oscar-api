ec97eab48cc91c9e76c34e619b3cbbac742ba797
Sam Noedel <sam.noedel@gmail.com>
2016-12-23 19:41:39 -0800

    Add user model unit tests

--- test/model/user.js
+++ test/model/user.js
@@ -1,11 +1,59 @@
 'use strict';
 
+const { User } = require('../../src/model/user'),
+  mongoose     = require('mongoose'),
+  { expect }   = require('code');
+
 describe('User model', () => {
-  it('should require name');
-  it('should not allow names < 3 characters');
-  it('should accept a valid name');
-  it('should require username');
-  it('should not allow usernames < 3 characters');
-  it('should restrict username characters');
-  it('should accept a valid username');
+  before(() => {
+    mongoose.Promise = Promise;
+  });
+
+  it('should require name', () => {
+    return new User({ }).validate(err => {
+      expect(err.errors.name).to.exist();
+    });
+  });
+
+  it('should not allow names < 3 characters', () => {
+    return new User({ name: 'ab' }).validate(err => {
+      expect(err.errors.name).to.exist();
+    });
+  });
+
+  it('should accept a valid name', () => {
+    return new User({ name: 'cheese' }).validate(err => {
+      expect(err.errors.name).not.to.exist();
+    });
+  });
+
+  it('should require username', () => {
+    return new User({ }).validate(err => {
+      expect(err.errors.username).to.exist();
+    });
+  });
+
+  it('should not allow usernames < 3 characters', () => {
+    return new User({ username: '12' }).validate(err => {
+      expect(err.errors.username).to.exist();
+    });
+  });
+
+  it('should restrict username characters', () => {
+    return new User({ username: '!@#$%^&*()-_=+' }).validate(err => {
+      expect(err.errors.username).to.exist();
+    });
+  });
+
+  it('should accept a valid username', () => {
+    return new User({ username: 'mister_cheese' }).validate(err => {
+      expect(err.errors.username).not.to.exist();
+    });
+  });
+
+  it('should pass validation with valid data', () => {
+    return new User({ name: 'foo', username: 'bar' }).validate(err => {
+      expect(err.errors).to.equal([ ]);
+    });
+  });
 });
