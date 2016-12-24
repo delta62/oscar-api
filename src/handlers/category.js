72a90bc4246d44cc43a7a0477b55a264be5ee2ef
Sam Noedel <sam.noedel@gmail.com>
2016-12-23 19:10:02 -0800

    Add category close handler

--- src/handlers/category.js
+++ src/handlers/category.js
@@ -1,10 +1,26 @@
 'use strict';
 
-const { Category } = require('../model/category');
+const restify  = require('restify'),
+  { Category } = require('../model/category');
 
 exports.browse = function categoryBrowseHandler(req, res, next) {
   Category.find({})
     .then(cats => res.json(cats))
+    .then(() => next())
+    .catch(err => next(err));
+};
+
+exports.put = function categoryPutHandler(req, res, next) {
+  if (!req.user.admin) {
+    throw new restify.UnauthorizedException();
+  }
+
+  Category.findOne({ name: req.body.name })
+    .then(cat => {
+      if (!cat) throw new restify.NotFoundError();
+      cat.set('closed', req.body.closed ? new Date() : null);
+      return cat.save();
+    })
     .then(() => next())
     .catch(err => next(err));
 };
