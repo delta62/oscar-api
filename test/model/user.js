0f8527ffa5992c04ff041f22c218536cce70fd55
Sam Noedel <sam.noedel@gmail.com>
2016-12-23 19:19:54 -0800

    Add test infrastructure

---
+++ test/model/user.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('User model', () => {
+  it('should require name');
+  it('should not allow names < 3 characters');
+  it('should accept a valid name');
+  it('should require username');
+  it('should not allow usernames < 3 characters');
+  it('should restrict username characters');
+  it('should accept a valid username');
+});
