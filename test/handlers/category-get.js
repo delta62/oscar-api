fa0c3b378a14a66225f5545645532d61d4ac67af
Sam Noedel <sam.noedel@gmail.com>
2016-12-27 13:24:17 -0800

    Add category GET tests

--- test/handlers/category-get.js
+++ test/handlers/category-get.js
@@ -1,6 +1,82 @@
+const request = require('supertest'),
+  api = require('../../src/api'),
+  jwt = require('jsonwebtoken'),
+  { expect } = require('code'),
+  { modelFactory } = require('../../src/model/model-factory'),
+  { categorySchema } = require('../../src/model/category');
+
 describe('GET /category', () => {
-  it('should return all of the categories');
-  it('should include the time closed categories were closed');
-  it('should include the options of each category');
-  it('should include the correct answer for categories');
+  let agent,
+    closed = new Date(2001, 0, 1),
+    token = jwt.sign({ admin: false }, 'secret');
+
+  before(() => {
+    return api.boot()
+      .do(api=> {
+        let Category = modelFactory(api.conn, categorySchema, 'Category');
+        return Category.remove({ })
+          .then(Category.create({
+            name: 'c1',
+            options: [ 'a', 'b', 'c' ],
+            closed,
+            answer: 'a'
+          }))
+          .then(Category.create({ name: 'c2', options: [ 'a' ] }));
+      })
+      .then(api => agent = request(api));
+  });
+
+  it('should respond with 401 when not logged in', done => {
+    agent
+      .get('/category')
+      .expect(401, done);
+  })
+
+  it('should respond with JSON', done => {
+    agent
+      .get('/category')
+      .set('Authorization', `Bearer ${token}`)
+      .expect('Content-Type', 'application/json')
+      .expect(200, done);
+  });
+
+  it('should return all of the categories', done => {
+    agent
+      .get('/category')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(res => expect(res.body.length).to.equal(2))
+      .expect(200, done);
+  });
+
+  it('should include the name of each category', done => {
+    agent
+      .get('/category')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(res => expect(res.body[0].name).to.equal('c1'))
+      .expect(200, done);
+  });
+
+  it('should include the time closed categories were closed', done => {
+    agent
+      .get('/category')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(res => expect(res.body[0].closed).to.startWith('2001-01-01'))
+      .expect(200, done);
+  });
+
+  it('should include the options of each category', done => {
+    agent
+      .get('/category')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(res => expect(res.body[0].options).to.equal([ 'a', 'b', 'c' ]))
+      .expect(200, done);
+  });
+
+  it('should include the correct answer for categories', done => {
+    agent
+      .get('/category')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(res => expect(res.body[0].answer).to.equal('a'))
+      .expect(200, done);
+  });
 });
