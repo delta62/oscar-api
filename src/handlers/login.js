c4fbc67413ae9cbc7369fc3da1f9e489bb0277f4
Sam Noedel <sam.noedel@gmail.com>
2017-01-05 22:05:45 -0800

    Align some =

--- src/handlers/login.js
+++ src/handlers/login.js
@@ -1,8 +1,8 @@
-const { userModelFactory } = require('../model/user');
-const { ensureFound } = require('../util');
+const { userModelFactory }  = require('../model/user');
+const { ensureFound }       = require('../util');
 const { UnauthorizedError } = require('restify');
-const { loginValidator } = require('../validators/login');
-const { signToken } = require('../services/login');
+const { loginValidator }    = require('../validators/login');
+const { signToken }         = require('../services/login');
 
 exports.post = function loginHandler(req, res, next) {
   let User = userModelFactory(req.conn);
