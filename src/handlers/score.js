9ff26ae042eef73813d80d610bba6e1c5151bf1b
Sam Noedel <sam.noedel@gmail.com>
2017-01-05 21:44:18 -0800

    Split score logic into service

--- src/handlers/score.js
+++ src/handlers/score.js
@@ -1,7 +1,7 @@
-const config = require('config');
-const { userModelFactory } = require('../model/user');
+const { userModelFactory }     = require('../model/user');
 const { categoryModelFactory } = require('../model/category');
 const { responseModelFactory } = require('../model/response');
+const { scoreCalculator }      = require('../services/score');
 
 exports.get = function scoreGetHandler(req, res, next) {
   let User = userModelFactory(req.conn);
@@ -9,33 +9,8 @@ exports.get = function scoreGetHandler(req, res, next) {
   let Response = responseModelFactory(req.conn);
 
   Promise.all([ User.find({ }), Category.find({ }), Response.find({ }) ])
-    .then(calculateScores)
+    .then(scoreCalculator)
     .then(res.json.bind(res))
     .then(next)
     .catch(next);
 };
-
-function calculateScores(promises) {
-  let [ users, categories, responses ] = promises;
-
-  return users.map(user => ({
-    username: user.username,
-    name: user.name,
-    score: calculateUserScore(user.username, categories, responses)
-  }));
-}
-
-function calculateUserScore(username, categories, responses) {
-  return responses.reduce((acc, res) => {
-    if (res.username !== username) {
-      return acc;
-    }
-
-    let category = categories
-      .find(cat => cat._id.toString() === res.category.toString());
-    let questionScore = res.value === category.answer
-      ? config.get('score.correct')
-      : config.get('score.incorrect');
-    return acc + questionScore;
-  }, 0);
-}
