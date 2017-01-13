8dc835ca0355ab09a36ed59fffa0ac7a657ecf34
Sam Noedel <sam.noedel@gmail.com>
2017-01-13 08:58:15 -0800

    Clean up server status endpoint

--- src/uptime.js
+++
@@ -1,7 +0,0 @@
-exports.uptimePlugin = function uptimePlugin(server) {
-  server.startedAt = +new Date();
-  server.uptime = function uptime() {
-    let now = +new Date();
-    return Math.floor((now - server.startedAt) / 1000);
-  };
-};
