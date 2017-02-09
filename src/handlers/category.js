0414e1aa305b5d0022cf950a36c4e8269eb3d592
Sam Noedel <sam.noedel@gmail.com>
2017-02-08 20:17:15 -0800

    Fix test & lint errors

--- src/handlers/category.js
+++ src/handlers/category.js
@@ -11,8 +11,10 @@ exports.categoryBrowseHandler = function categoryBrowseHandler(req, res, next) {
 exports.categoryPatchHandler = function categoryPatchHandler(req, res, next) {
   categoryPatchValidator(req)
     .then(model => {
+      let id = req.params.id;
       return Promise.resolve()
-        .then(() => this.models.Category.findByIdAndUpdate(req.params.id, model))
+        .then(() => this.models.Category.findByIdAndUpdate(id, model))
+        .do(ensureFound)
         .do(original => {
           if (model.closed && !original.closed) {
             this.sockets.emit('categoryClosed', { categoryId: original._id });
@@ -21,9 +23,11 @@ exports.categoryPatchHandler = function categoryPatchHandler(req, res, next) {
             this.sockets.emit('categoryOpened', { categoryId: original._id });
           }
           if (model.answer !== original.answer) {
-            this.sockets.emit('categoryAnswered', { categoryId: original._id, answer: model.answer });
+            this.sockets.emit('categoryAnswered', {
+              categoryId: original._id, answer: model.answer
+            });
           }
-        })
+        });
     })
     .do(ensureFound)
     .then(res.json.bind(res))
